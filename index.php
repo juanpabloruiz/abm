edcd43b152c8e78dde9abfc8f06c8a468369e484
juanpabloruiz <pabloruiz1980@gmail.com>
2023-05-08 13:45:05 -0300

    Primeros archivos agregados

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página</title>
+</head>
+<body>
+    <nav>
+        <a href="">Inicio</a>
+        <a href="">Contacto</a>
+        <a href="./panel">Administración</a>
+    </nav>
+    <header>
+        <h1>Título</h1>
+        <h2>Subtítulo</h2>
+    </header>
+    <main>
+        Contenido
+    </main>
+    <footer>
+        Píe de página &copy <?php echo date('Y'); ?>
+    </footer>
+</body>
+</html>
